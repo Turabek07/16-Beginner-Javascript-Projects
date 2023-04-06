4abf63ec61017eb1c40b0f9925401a087f069175
Turabek <turabekyusubov587@gmail.com>
2023-04-06 20:26:59 +0500

    Completed!

--- Multiplication App/app.js
+++ Multiplication App/app.js
@@ -1,31 +1,35 @@
-const num1 = Math.ceil( Math.random() * 10 ) ;
-const num2 = Math.ceil( Math.random() * 10 ) ;
+const num1 = Math.ceil(Math.random() * 10);
+const num2 = Math.ceil(Math.random() * 10);
 
 const questionEl = document.getElementById("question");
 const inputEl = document.getElementById("input")
 const formEl = document.getElementById("form")
-// const scoreEl = document.getElementById("score")
+const scoreEl = document.getElementById("score")
 
-let score = JsON.parse(localStorage.getItem("score")); 
+let score = JSON.parse(localStorage.getItem("score"));
 
-if(!score){
+
+if (!score) {
     score = 0;
 }
+scoreEl.innerText = `score:${score}`
 
 questionEl.innerText = `What is the ${num1} multiply by ${num2}?`
 
 const correctAns = num1 * num2;
 
 formEl.addEventListener("submit", () => {
- const userAns = +inputEl.value;
- if(userAns === correctAns){
-  score++;
- }else{
-    score--;
- }
+    const userAns = +inputEl.value;
+    if (userAns === correctAns) {
+        score++;
+        updateLocalStorage();
+    } else {
+        score--;
+        updateLocalStorage();
+    }
 })
-// scoreEl.innerHTML = `score:${score}`
 
-function updateLocalStorage(){
-    localStorage.setItem("score",JSON.stringify(score))
+
+function updateLocalStorage() {
+    localStorage.setItem("score", JSON.stringify(score))
 }
